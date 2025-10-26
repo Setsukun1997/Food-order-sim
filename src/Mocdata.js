ee6ba3cf17c1d0eafe9ab8147eb8351ca706f633
Setsukun1997 <tety12308@hotmail.com>
2025-10-26 12:24:47 +0700

    Create Mocdata.js

---
+++ src/Mocdata.js
@@ -0,0 +1,20 @@
+export const menuItems = [
+  {
+    id: 1,
+    name: "ข้าวผัดกุ้ง",
+    price: 60,
+    image: "/images/fried-rice.jpg"
+  },
+  {
+    id: 2,
+    name: "ผัดไทย",
+    price: 55,
+    image: "/images/pad-thai.jpg"
+  },
+  {
+    id: 3,
+    name: "ต้มยำกุ้ง",
+    price: 70,
+    image: "/images/tomyum.jpg"
+  }
+];
