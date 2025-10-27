6c1bdff2ec60507b0f6ce0a8e053d831ba2bcde2
Setsukun1997 <tety12308@hotmail.com>
2025-10-27 13:24:17 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <Routes>
 
-        <Route path="/" element={isLoggedIn() ? <Menu /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Menu />} />
 
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
 
