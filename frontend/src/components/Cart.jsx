fbcd96bb50a521ef08b90c93ca788ebd4dbd5f32
Setsukun1997 <tety12308@hotmail.com>
2025-10-27 14:05:26 +0700

    Update Cart.jsx

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,6 +1,7 @@
+import './styles/Cart.css';
 export default function Cart() {
   return (
-    <div>
+    <div className="cart-container">
       <h2>ตะกร้าของคุณ</h2>
       <p>ยังไม่มีรายการอาหาร</p>
     </div>
