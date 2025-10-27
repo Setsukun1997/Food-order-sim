602dad2870a012cdbe2f18b7c29baabc3b30e56d
Setsukun1997 <tety12308@hotmail.com>
2025-10-27 14:36:14 +0700

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -9,23 +9,26 @@ export default function Signup() {
 
   const handleSignup = async () => {
     try {
-      const res = await fetch('https://food-order-backend-b401.onrender.com/api/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
-      });
-      const data = await res.json();
-      if (data.message === 'User registered') {
-        alert('สมัครสมาชิกสำเร็จ');
-        navigate('/login');
-      } else {
-        alert('สมัครสมาชิกไม่สำเร็จ');
-      }
-    } catch (err) {
-      console.error('Signup error:', err);
-      alert('เกิดข้อผิดพลาด');
+   const handleSignup = async () => {
+  try {
+    const res = await fetch('https://food-order-backend-b401.onrender.com/api/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    });
+    const data = await res.json();
+    console.log(data); 
+    if (data.message === 'User registered') {
+      alert('สมัครสมาชิกสำเร็จ');
+      navigate('/login');
+    } else {
+      alert('สมัครสมาชิกไม่สำเร็จ');
     }
-  };
+  } catch (err) {
+    console.error('Signup error:', err);
+    alert('เกิดข้อผิดพลาด');
+  }
+};
 
   return (
     <div className="signup-container">
