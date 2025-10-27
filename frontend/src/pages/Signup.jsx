9bf2c2d7212172cbc857e54f87a260708a43c160
Setsukun1997 <tety12308@hotmail.com>
2025-10-27 14:40:59 +0700

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import '../styles/signup.css';
+import '../styles/Signup.css';
 
 export default function Signup() {
   const [email, setEmail] = useState('');
