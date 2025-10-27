35db5749c8bfc9ca062597fc9665de0214a16abb
Setsukun1997 <tety12308@hotmail.com>
2025-10-27 12:43:12 +0700

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login({ onLogin }) {
     const navigate = useNavigate();
