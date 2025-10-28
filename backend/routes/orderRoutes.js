c19f4291a4dddd6a2d0e36d11bc0a78c2968038f
Setsukun1997 <tety12308@hotmail.com>
2025-10-28 16:30:47 +0700

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,6 @@ const { createOrder, getOrder } = require('../controllers/orderController');
 const router = express.Router();
 
 router.post('/', createOrder);
-router.get('/order', getOrders);
+router.get('/order', getOrder);
 
 module.exports = router;
