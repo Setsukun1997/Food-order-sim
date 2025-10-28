a4887e4a512143bf297f06091a955ec8a45defbd
Setsukun1997 <tety12308@hotmail.com>
2025-10-28 10:29:54 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: String,
+  password: String,
+  name: String,
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
-
 
