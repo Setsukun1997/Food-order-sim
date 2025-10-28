127b726577222c765d869b3d1ba2d0b891f5df96
Setsukun1997 <tety12308@hotmail.com>
2025-10-28 15:38:59 +0700

    Update Cart.jsx

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import '../styles/Cart.css';
 function Cart() {
   const navigate = useNavigate();
   const [cart, setCart] = useState(() => {
