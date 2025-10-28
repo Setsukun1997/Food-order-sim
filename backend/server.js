45b1ad77e44ff98f0230048b7c8b8b86b4705da6
Setsukun1997 <tety12308@hotmail.com>
2025-10-28 10:40:20 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1);
   });
 app.use('/api/auth', require('./routes/auth')); 
-app.use('/api/orders', require('./routes/orders')); 
+app.use('/api/orders', require('./routes/order')); 
 app.get('/', (req, res) => {
   res.send('🍽️ Food-order backend is running!');
 });
